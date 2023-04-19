fe69907f918d88f068b2f3f4808ab2b2d5701872
Charles Chan <charleschan1108@gmail.com>
2023-04-20 00:37:27 +0800

    fixed madness of bot

--- app/chatbot.py
+++ app/chatbot.py
@@ -3,27 +3,40 @@ import streamlit as st
 from streamlit_chat import message
 import os
 
-openai.api_key = st.secrets["OPENAI_API_KEY"]
-gpt_model = st.secrets["MODEL"]
+openai.api_key = os.environ.get("OPENAI_API_KEY") #st.secrets["OPENAI_API_KEY"]
+gpt_model = "text-davinci-003" #st.secrets["MODEL"]
+
+def contextualize():
+    # Prompt engineering
+    prompt = "Summary: The user, Sonal, is a working professional, looking to form good habits. You are Joy, a psychologist, trying to help Sonal manage stress." \
+                    " Specific information: Sonal's stressor is work"\
+                    "\nIntroduce yourself as a professional psychologist" + "\n\nAI:" 
+    completions = openai.Completion.create(model=gpt_model, 
+                                        prompt=prompt,
+                                        temperature = 0.89,
+                                        max_tokens=256,
+                                        top_p = 1,
+                                        frequency_penalty = 0,
+                                        presence_penalty=0.6,
+                                        stop=["Human:", "AI:"])
+    
+    message = completions["choices"][0]["text"]
+    return message.lstrip()
 
 def generate_response(query: str):
-    prompt = "The following is a conversation with a therapist and a user." \
-                " The therapist is JOY, who uses compassionate listening to have helpful and meaningful conversations with users. "\
-                "JOY is empathic and friendly. JOY's objective is to make the user feel better by feeling heard. "\
-                "With each response, JOY offers follow-up questions to encourage openness and tries to continue the conversation in a natural way. \n\n"\
-                "JOY-> Hello, I am your personal mental health assistant. What's on your mind today?\nUser->"+ query + "JOY->"
-
+    prompt = "Summary: You are trying to help user to manage stress." \
+                " Specific information: user's stressor is work\n\nHuman:" + query + "\nAI:"
     completions = openai.Completion.create(model=gpt_model, 
                                     prompt=prompt,
                                     temperature = 0.89,
-                                    max_tokens=162,
+                                    max_tokens=256,
                                     top_p = 1,
                                     frequency_penalty = 0,
                                     presence_penalty=0.6,
-                                    stop=["\n"])
+                                    stop=["Human:", "AI:"])
     
     message = completions["choices"][0]["text"]
-    return message
+    return message.lstrip()
 
 #Creating the chatbot interface
 st.title("chatBot : Streamlit + openAI")
@@ -35,20 +48,31 @@ if 'generated' not in st.session_state:
 if 'past' not in st.session_state:
     st.session_state['past'] = []
 
+if 'context' not in st.session_state:
+    starter = contextualize()
+    st.session_state['context'] = [starter]
+
+# Contextualize chat bot
+if st.session_state['context']:
+    message(st.session_state['context'][0])
+
+
+
 # We will get the user's input by calling the get_text function
 def get_text():
     # input_text = st.text_input("You: ","Hello, how are you?", key="input")
-    input_text = st.text_input("Talk to our virutal therapist Joy", disabled=False, placeholder="What's on your mind?")
+    input_text = st.text_input("Talk to our virtual psychologist Joy.", disabled=False, placeholder="What's on your mind?")
     return input_text
 
 user_input = get_text()
+
 
 if user_input:
     output = generate_response(user_input)
     # store the output 
     st.session_state.past.append(user_input)
     st.session_state.generated.append(output)
-
+    
 if st.session_state['generated']:
     for i in range(len(st.session_state['generated'])-1, -1, -1):
         message(st.session_state["generated"][i], key=str(i))
